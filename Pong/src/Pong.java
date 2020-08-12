522992b95b302809e808c9638545005f0af72286
mcfudd <christian.mans@gmail.com>
2020-08-12 16:11:02 +1200

    set up JFrame

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,24 @@
+import javax.swing.JFrame;
 
-public class Pong {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Pong extends JFrame
+{
+	// set magic numbers
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+		
+	public Pong()
+	{
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args)
+	{
+		new Pong();
 	}
 
 }
